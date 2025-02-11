6b705bdcbc9a7487098bc8e6659052da333e768f
Miguel Ángel Bernal Sánchez <mbersan1005@g.educaand.es>
2025-02-11 08:59:54 +0100

    Algunos Ficheros modificados

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+'name': 'Alquiler de Productos',
+'version': '1.0',
+'author': 'Miguel Ángel Bernal Sánchez',
+'category': 'Custom',
+'summary': 'Gestión de Alquiler de productos',
+'depends': ['base'],
+'data': [
+    'security/ir.model.access.csv',
+    'views/alquiler_producto_views.xml',
+],
+    'icon': '/alquiler_producto/static/description/icon55.png',
+    'installable': True,
+    'application': True,
+}
---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from . import alquiler_producto
